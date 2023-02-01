b62e5c4051ee2dd9ada6efa97d66f6907677dc06
roadranmee-orangecuk <1289468324@qq.com>
2023-02-01 22:11:35 +0800

    调用了发送请求和解析的方法。

--- check.py
+++ check.py
@@ -2,6 +2,9 @@ import requests
 from lxml import etree
 import json
 
+from .res import res_get_try
+from .res import tree_xpath_parse
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 }
@@ -10,7 +13,9 @@ headers = {
 def cc() -> str:
     url = 'https://vapi.cc.163.com/video_play_url/361433'
     # 该接口返回一个包含直播信息的json字符串，包含名为data的键且当其值为no live时则直播未开始
-    res_status = requests.get(url=url, headers=headers, timeout=3)
+    res_status = res_get_try(url=url, proxies={}, timeout=(3, 15), max_try=3)
+    if res_status is None:
+        return '不知道勃没勃，反正我摸了'
     res_status.encoding = 'utf-8'
     json_status = res_status.text
     dict_status = json.loads(json_status)
@@ -19,11 +24,12 @@ def cc() -> str:
             return '没勃，摸了'
     except KeyError:
         url = 'https://cc.163.com/361433/'
-        res_title = requests.get(url=url, headers=headers, timeout=3)
+        res_title = res_get_try(url=url, proxies={},
+                                timeout=(3, 15), max_try=3)
+        if res_title is None:
+            return '不知道勃没勃，反正我摸了'
         res_title.encoding = 'utf-8'
         html = res_title.text
-        hparser = etree.HTMLParser()
-        tree = etree.HTML(html, hparser)
-        title = tree.xpath(
-            '//div[@class="js-onlive-title-normal nick onlive-setting-name"]/text()')[0]
+        xpath_ = '//div[@class="js-onlive-title-normal nick onlive-setting-name"]/text()'
+        title = tree_xpath_parse(html=html, xpath_=xpath_)[0]
         return '勃了勃了，'+title.replace('【Quin】', '')
