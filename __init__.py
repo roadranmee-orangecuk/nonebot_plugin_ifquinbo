42e2acef80e14830e761fd85959f4ff8df6a14e3
roadranmee-orangecuk <1289468324@qq.com>
2023-02-03 20:44:12 +0800

    删除了没有使用的模块和参数。

--- __init__.py
+++ __init__.py
@@ -1,12 +1,11 @@
 from .check import cc
 from nonebot.plugin import on_keyword
-from nonebot.adapters.onebot.v11 import Bot, Event
 from nonebot.adapters.onebot.v11.message import Message
 
 ifquinbo = on_keyword(['奎恩哥勃了没'])
 
 
 @ifquinbo.handle()
-async def ifquinbo_handle(bot: Bot, event: Event):
+async def _():
     msg = cc()
     await ifquinbo.finish(Message(msg))
